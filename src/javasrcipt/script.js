871439c9bd5ac9afa7281634830b394a99546d22
Kaueaferreira <kaue.arcanjo04@gmail.com>
2024-11-05 00:29:00 -0300

    Finalizando Projeto Ultimo commit

--- src/javasrcipt/script.js
+++ src/javasrcipt/script.js
@@ -1,8 +1,63 @@
-
-
 $(document).ready(function() {
     $('#mobile_btn').on('click', function () {
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find('i').toggleClass('fa-x');
     });
+
+    const sections = $('section');
+    const navItems =$('.nav-item');
+
+    /* O comando a baixo é para fazer uma box-shadow, quando scroll rolar para baixo */
+
+    $(window).on('scroll', function(){
+        const header = $('header'); 
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+        let activeSecctionIndex = 0;
+
+        if(scrollPosition <= 0) {
+            header.css('box-shadow', 'none');
+        }
+        else{
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0 ,0 , 0.1');
+        }
+
+        sections.each(function(i) {
+            const section = $(this)
+            const sectionTop = section.offset().top - 96;
+            const sectionBottom = sectionTop+ section.outerHeight();
+
+            if(scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                activeSecctionIndex = i;
+                return false;
+            }
+
+        })
+        navItems.removeClass('active');
+        $(navItems[activeSecctionIndex]).addClass('active');
+    });
+
+    ScrollReveal().reveal('#cta', {
+        origin: 'left', 
+        durantion: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('.dish', {
+        origin: 'left', 
+        durantion: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('#testimonials_chef', {
+        origin: 'left', 
+        durantion: 1000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('.feedback', {
+        origin: 'rigth', 
+        durantion: 1000,
+        distance: '20%'
+    })
 });
